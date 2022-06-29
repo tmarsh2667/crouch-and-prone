028b457ae0a72bebd1105eaf4dfdfb8b8f0bb24b
tmarsh2667 <88873826+tmarsh2667@users.noreply.github.com>
2022-06-30 01:53:48 +1000

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,11 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+client_scripts {
+	'Crouch.Client.net.dll',
+	'Common.Client.net.dll'
+}
+
+server_scripts {
+	'Crouch.Server.net.dll',
+	'Common.Server.net.dll'
+}
